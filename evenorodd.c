fcfe06370602cdf95b8858cf505bf47327bd42cf
nikiraj <nikiraj3007@gmail.com>
2016-12-23 22:34:41 -0800

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int n;
+ printf("enter the number:");
+ scanf("%d",&n);
+ if(n%2==0)
+ printf("\nthe number is even");
+ else
+ printf("\nthe number is odd");
+ getch();
+ }
